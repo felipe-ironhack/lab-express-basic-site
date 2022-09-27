f4e76821d164b4c81d2c1b586176bd23a48ca8c5
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-09-27 11:07:00 +0200

    added more hints regarding the styles

--- app.js
+++ app.js
@@ -5,13 +5,21 @@
 => npm install express
 
 => npm i --save-dev nodemon 
+  (if you have installed globally you can also skip that step)
 
 => run nodemon app.js 
   (you need to be inside the folder that contains app.js file)
   that you you can see the updates live
 
-=> dirname takes 2 underscores _ _ __dirname
+=> dirname takes 2 underscores _ _ => __dirname
 
-=> remember to have /views/
+=> remember to have "/views/<name of you file here>"
+
+=> the images and css can use 
+    => <link rel="stylesheet" href="/stylesheets/<name of you file here>" />
+    => <img src ='/images/<name of you file here> />'
+    => background-image: url('/images/<name of you file here>');
+
+=> Some CSS changes can may need to refresh the page to be applied
 
 */
