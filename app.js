fc280e926e5874d55edf1fdb237f958caf6870c9
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-09-27 10:35:02 +0200

    some starting hints

---
+++ app.js
@@ -0,0 +1,17 @@
+/*
+
+=> npm init -y
+
+=> npm install express
+
+=> npm i --save-dev nodemon 
+
+=> run nodemon app.js 
+  (you need to be inside the folder that contains app.js file)
+  that you you can see the updates live
+
+=> dirname takes 2 underscores _ _ __dirname
+
+=> remember to have /views/
+
+*/
